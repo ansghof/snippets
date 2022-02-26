2b63be55325112cbb46dd19df7c9812429ab9836
Ansgar Hoffmann <ansgar.hoffmann@daimler.com>
2022-02-26 09:48:16 +0100

    add general mocking, change folder structure

---
+++ testing/jest/mocks/general/mocking.ts
@@ -0,0 +1,29 @@
+// 1. without any import before, directly mock the packages one method and let it return the desired object
+jest.mock("@ansghof/my-service", () => ({
+  getData: jest.fn(() =>
+    Promise.resolve({
+      id: "asdf1234",
+      given_name: "Ansgar",
+      family_name: "Hoffmann",
+      user_type: "tester",
+      email: "test@test.io",
+    })
+  ),
+}));
+
+// 2. as jest.Mock
+// import method from package,
+// assign it as jest.Mock to a constant.
+// use .mockResolvedValue() to set a mock return value
+import { getConfig } from "@ansghof/my-service";
+
+const getConfigMock = getConfig as jest.Mock;
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+});
+
+it("should make getConfigMock return config", async () => {
+  getConfigMock.mockResolvedValueOnce(config);
+  // confinue with test
+});

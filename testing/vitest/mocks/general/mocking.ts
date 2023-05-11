b7394cdee37f38acd3c8ff385f06692eb55f58ae
Ansgar Hoffmann <78479501+ansghof@users.noreply.github.com>
2023-05-11 13:29:42 +0200

    Create mocking.ts

---
+++ testing/vitest/mocks/general/mocking.ts
@@ -0,0 +1,20 @@
+import * as myService from '@xxx/myservice';
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+it('should do ...', async () => {
+    const nameSpy = vi.spyOn(portalService, 'getName');
+    nameSpy.mockResolvedValueOnce(mockUsername);
+
+    const wrapper = mount(MyComponent, {
+      props: {
+      },
+    });
+    // needs to be there:
+    await flushPromises();
+    
+    expect(nameSpy).toHaveBeenCalledTimes(1);
+    const nameElement = wrapper.find('blabla');
+    expect(nameElement.text()).equals(fullName);
+}
